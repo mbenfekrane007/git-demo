e1196ad3e32f6a82d0ce1f810a69147ef2996998
mbenfekrane <mbenfekrane@sqli.com>
2022-03-14 18:25:06 +0100

    [feat]CALC-2

---
+++ src/main/resources/main.java
@@ -0,0 +1,13 @@
+
+
+
+public class main {
+    public static void main(String []args){
+        Calc calcul = new Calc();
+        System.out.println(String.valueOf(calcul.add(5,3)));
+        System.out.println(String.valueOf(calcul.sub(5,3)));
+        System.out.println(String.valueOf(calcul.mult(5,3)));
+        System.out.println(String.valueOf(calcul.devision(5,3)));
+
+    }
+}
